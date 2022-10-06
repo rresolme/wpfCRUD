935957e3b8d37c8e95c398c10179b77a519b41de
rresolme <rexjordanresolme@gmail.com>
2022-10-07 00:08:49 +0800

    initial commit

---
+++ WpfEntityCRUD/InsertPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfEntityCRUD
+{
+    /// <summary>
+    /// Interaction logic for InsertPage.xaml
+    /// </summary>
+    public partial class InsertPage : Window
+    {
+        wpfCrudEntities _db = new wpfCrudEntities();
+
+        public InsertPage()
+        {
+            InitializeComponent();
+        }
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            citizen newCitizen = new citizen()
+            {
+                name = nametextBox.Text,
+                ageGroup = ageGroupcomboBox.Text
+            };
+
+            _db.citizens.Add(newCitizen);
+            _db.SaveChanges();
+            MainWindow.datagrid.ItemsSource = _db.citizens.ToList();
+            this.Hide();
+        }
+    }
+}
---
+++ WpfEntityCRUD/UpdatePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfEntityCRUD
+{
+    /// <summary>
+    /// Interaction logic for UpdatePage.xaml
+    /// </summary>
+    public partial class UpdatePage : Window
+    {
+        wpfCrudEntities _db = new wpfCrudEntities();
+        int Id;
+
+        public UpdatePage(int citizenId)
+        {
+            InitializeComponent();
+            Id = citizenId;
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            citizen updateCitizen = (from m in _db.citizens
+                                  where m.id == Id
+                                  select m).Single();
+            updateCitizen.name = nametextBox.Text;
+            updateCitizen.ageGroup = ageGroupcomboBox.Text;
+            _db.SaveChanges();
+            MainWindow.datagrid.ItemsSource = _db.citizens.ToList();
+            this.Hide();
+        }
+    }
+}
